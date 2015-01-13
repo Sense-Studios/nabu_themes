b350951c43bf21558ca8fed406d60d0ac0e1208d
Sense <urias@sense-studios.com>
2015-01-13 15:15:48 +0100

    well now

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -2,5 +2,5 @@
 # create extra menu items
 
 EXTRA_ADMIN_MENU_ITEMS.push( { "title" => "Themes Engine", "header" => true } )
-EXTRA_ADMIN_MENU_ITEMS.push( { "title" => "Theme-Menu", "link" => "/theme/menu", "glyphicon" => "glyphicon-cog" } )
+EXTRA_ADMIN_MENU_ITEMS.push( { "title" => "Theme-Menu", "link" => "/theme/menu", "glyphicon" => "glyphicon-list" } )
 EXTRA_ADMIN_MENU_ITEMS.push( { "title" => "Themes", "link" => "/theme/themes", "glyphicon" => "glyphicon-cog" } )
